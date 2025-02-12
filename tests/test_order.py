1f4957e4cb6541c0c45c16a3e44fa873a39e4718
Camelia Ciuca <camelia1414@gmail.com>
2025-02-12 14:19:02 +0100

    order & test_order all test failed

--- tests/test_order.py
+++ tests/test_order.py
@@ -1,2 +1,21 @@
-def test_order():
+
+
+
+def test_get_id_order():
+    assert False
+
+
+def test_get_order_info():
+    assert False
+
+
+def test_confirm_order():
+    assert False
+
+
+def test_cancel_order():
+    assert False
+
+
+def test_print_order_info():
     assert False
